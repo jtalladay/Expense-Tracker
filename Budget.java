25e7033a2f47d916e005daa6e0509baec0ddb06f
jared <j.talladay@gmail.com>
2023-10-11 22:54:24 -0400

    - added budgetSetup method - added mainMenu method - added expenseAdder method

--- Budget.java
+++ Budget.java
@@ -7,13 +7,29 @@ import java.util.Scanner;
 // setup percent values for each category
 public class Budget {
 
+    public static void budgetSetup() {
+        Scanner userInput = new Scanner(System.in);
 
-    private static int numberCategories;
-    public static String[] categories;
+        System.out.print("Enter the number of categories: ");
+        int numCategories = userInput.nextInt();
+        userInput.nextLine(); // Consume the newline character
 
-    public Budget(int cat) {
-        numberCategories = cat;
-        String[] categories = new String[numberCategories];
+        String[] stringArray = new String[numCategories];
+
+        for (int i = 0; i < numCategories; i++) {
+            System.out.print("Enter string " + (i + 1) + ": ");
+            stringArray[i] = userInput.nextLine();
+        }
+
+        // Now, you have an array of strings in stringArray.
+
+        // Print the entered strings
+        System.out.println("You entered the following categories:");
+        for (String str : stringArray) {
+            System.out.println(str);
+        }
+
+        //userInput.close(); // Close the scanner when done.
     }
 }
 
--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -3,13 +3,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
- /* what are we trying to do.
- main menu:
- 1) Setup New Budget
- 2) Restore Budget
- 3) Expense Adding Mode
- 4) Restore previous expense tracker
+// Major issue, how can i save a list of categories in the budget object that
+// can be used in the expensetracker object
 
+ /* STILL TO DO
  Setup New Budget
  Name new budget: "Home Budget"
  Enter what categories you want "Car, Grocery"
@@ -29,26 +26,6 @@ import java.util.Scanner;
  1) "Home Budget"
  "1" enter
 
- Expense Adder Mode
- Enter your selection:
- 1) Add new expense
- 2) View total expenses
- 3) clear expenses
- "1" enter
-
-Enter the date in the format yyyy-MM-dd
- "2023-12-25"
-Enter the category from list:
-Car, Grocery
-"Car"
-Enter Amount
-
-repeats:
-Enter your selection:
-objects:
-expense
-budget
-expense tracker
 
 methods
 mainMenu()
@@ -74,80 +51,126 @@ public class ExpenseTracker {
 
     public static void main(String[] args){
 
-
-        String selection = "";
         System.out.println("Expense Tracker Program");
         mainMenu();
 
 
-        while (!selection.equalsIgnoreCase("X")) {
-            System.out.println("Make your selection: ");
-            System.out.println("A: Add expense");
-            System.out.println("B: View total expenses");
-            System.out.println("C: Clear totals");
-            selection = userInput.nextLine();
+    }//end of main
 
-            System.out.println("You selected " + selection);
+     /*
+    main menu:
+            1) Setup New Budget
+            2) Restore Budget
+            3) Expense Adding Mode
+            4) Restore previous expense tracker
+    */
+     public static void mainMenu(){
+         int selection = 0;
+         while(selection != 5) {
+             System.out.println("Main Menu: \n 1) Setup New Budget \n 2) Restore Budget \n " +
+                     "3) Expense Adding Mode \n 4) Restore previous expense tracker \n " +
+                      "5) QUIT");
+             selection = userInput.nextInt();
 
-
-            switch (selection.toUpperCase()) {
-                case "A":
-
-                    System.out.println("Enter the date in the format yyyy-MM-dd ");
-                    String date = userInput.nextLine();
-                    System.out.println("Enter the category from list: \n" + Arrays.toString(Budget.categories));
-                    String category = userInput.nextLine();
-                    System.out.println("Enter the amount to add to the total: ");
-                    double amount = userInput.nextDouble();
-
-                    expenses.add(new Expense(id, date, category, amount));
-                    id++;
-                    break;
-
-                case "B":
-                    System.out.println(expenses);
-                    System.out.println("Total = " + numberFormatter.format(total));
-                    break;
-
-                case "C":
-                    clearTotal();
-            }
-        }
-    }
-    public static void mainMenu(){
-        System.out.println("Main Menu: \n 1) Setup Budget \n 2) Expense Adder \n ");
-        int selection = userInput.nextInt();
-        switch (selection){
-            case 1:
-                budgetSetup();
-                break;
-            case 2:
-                System.out.println("Returning");
-                break;
-        }
-    return;
-    }
-     public static void budgetSetup(){
-//  is this where load existing budget will happen?
-
-         System.out.println("Enter number of categories: ");
-         int numberCategories = userInput.nextInt();
-         System.out.println("Enter " + numberCategories + "categories followed by enter: ");
-         Budget budget = new Budget(numberCategories);
-
-/*
-         int i = 0;
-         while (i < categories.length) {
-             categories[i] = userInput.nextLine();
-             i++;
+             switch (selection) {
+                 case 1:
+                     budgetSetup();
+                     break;
+                 case 2:
+                     System.out.println("Restoring previously used budget... ");
+                     break;
+                 case 3:
+                     System.out.println("Entering expense mode... ");
+                     expenseAdder();
+                     break;
+                 case 4:
+                     System.out.println("Resotring previously used expense tracker...");
+                     break;
+                 case 5:
+                     break;
+             }
+             //in the while loop still
          }
-         System.out.println("You entered: " + Arrays.toString(categories));
+         return;
+     }//end of mainMenu
+/*
+     Expense Adder Mode
+     Enter your selection:
+        1) Add new expense
+        2) View total expenses
+        3) clear expenses
+        "1" enter
+
+     Enter the date in the format yyyy-MM-dd
+        "2023-12-25"
+     Enter the category from list:
+     Car, Grocery
+        "Car"
+     Enter Amount
+
+     repeats:
+     Enter your selection:
+
 */
+     public static void expenseAdder(){
+
+         int selection = 0;
+
+         while (selection != 4) {
+             System.out.println("Expense Adder Menu: \n 1) Add Expense \n 2) View total expenses\n " +
+                     "3) Clear totals\n 4) Quit \n ");
+             selection = userInput.nextInt();
+
+             switch (selection) {
+                 case 1:
+                     System.out.println("Enter the date in the format yyyy-MM-dd ");
+                     String date = userInput.nextLine();
+                     //System.out.println("Enter the category from the list: \n" + Arrays.toString(Budget.categories));
+                     System.out.println("Enter the category from the list: ");
+                     String category = userInput.nextLine();
+                     System.out.println("Enter the amount to add to the total: ");
+                     double amount = userInput.nextDouble();
+                     userInput.nextLine(); // Consume the newline character
+                     expenses.add(new Expense(id, date, category, amount));
+                     id++;
+                     break;
+
+                 case 2:
+                     System.out.println(expenses);
+                     System.out.println("Total = " + numberFormatter.format(total));
+                     break;
+
+                 case 3:
+                     clearTotal();
+                     break;
+                 case 4:
+                     System.out.println("Quitting expense adder mode...");
+                     break;
+             }
+
+         }
+     }// end of expenseAdder
+
+     public static void budgetSetup(){
+//  is this where load existing budget will happen? yes
+// do you want to set up a new budget or restore a saved budget from file
+         // change to say create new or load existing.
+         System.out.println("New budget? 0 for no 1 for yes: ");
+         int input = userInput.nextInt();
+
+         switch(input){
+            case 0:
+                break;
+            case 1:
+                Budget budget = new Budget();
+                budget.budgetSetup();
+                break;
+        }
+        return;
      }
 
     public static void clearTotal() {
         ExpenseTracker.total = 0;
     }
-
 
 }
